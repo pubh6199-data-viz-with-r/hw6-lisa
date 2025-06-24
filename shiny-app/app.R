1e024bb5bf938a3cb3382448d47766f41475e21c
Molayo Ifebajo <molayoifebajo@Molayos-MacBook-Air-2.local>
2025-06-24 05:15:04 -0400

    Cleaned dataset and updated variable names in app.R

--- shiny-app/app.R
+++ shiny-app/app.R
@@ -1,30 +1,41 @@
-<<<<<<< Updated upstream
+library(stringr)
+library(dplyr)
 # global.R
-data <- readr::read_csv("data/train.csv")
-  
+
+#load data
+data <- readr::read_csv("data/train.csv") 
+
+#clean data by fixing mismatch in rows
+data_clean <- data %>%
+  mutate(
+    is_misplaced = str_to_lower(Age) %in% c("male", "female", "non-binary"),
+    true_age = if_else(is_misplaced, Gender, Age),
+    true_gender = if_else(is_misplaced, Age, Gender),
+    Age = as.numeric(true_age),
+    Gender = true_gender
+  ) %>%
+  select(-true_age, -true_gender, -is_misplaced)
+
+#drop problematic row with na values for age and gender
+data_clean <- data_clean %>%
+  filter(!is.na(Age))
+#rename columns
+data_clean <- data_clean %>%
+  rename(
+    user_id = User_ID,
+    age = Age,
+    gender = Gender,
+    platform = Platform,
+    usage_mins = `Daily_Usage_Time (minutes)`,
+    posts_daily = Posts_Per_Day,
+    likes_daily = Likes_Received_Per_Day,
+    comments_daily = Comments_Received_Per_Day,
+    messages_daily = Messages_Sent_Per_Day,
+    emotion = Dominant_Emotion
+  )
+
 # ui.R
   
 # server.R
-=======
-library(shiny)
-library(tidyverse)
-library(lubridate)
-library(forcats) 
 
-# Load the data
-nsf_terminations <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-05-06/nsf_terminations.csv')
-
-# TODO: Preprocess the data
-# convert grant_number to character
-# collapse division column to only keep the 9 most frequent divisions and a catch-all category called Other
-# collapse nsf_program_name column to only keep the 9 most frequent divisions and  and a catch-all category called Other
-# your code here:
-nsf_data <- nsf_terminations %>%
-  mutate(
-    grant_number = as.character(grant_number),
-    division = fct_lump(fct_infreq(division), n = 9),
-    nsf_program_name = fct_lump(fct_infreq(nsf_program_name), n = 9))
-
-
->>>>>>> Stashed changes
 
